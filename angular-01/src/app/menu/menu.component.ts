571f8a6583f57b55b37b940d845673703157d32e
ManuelHRdg <42795594+ManuelHRdg@users.noreply.github.com>
2023-03-03 09:25:25 -0600

    Source de la practica angular-01
    Archivos del proyecto de angular-01 sin los modules

---
+++ angular-01/src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  opciones :Array<String>;
+
+  constructor(){
+    this.opciones = ['inicio', 'acerca', 'contacto']
+  }
+
+}
