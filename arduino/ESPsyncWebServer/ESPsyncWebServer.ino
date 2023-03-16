21a56100948110c6e49a94f2ffd57aa57db2cbb0
ManuelHRdg <manuel.gat1@gmail.com>
2023-03-16 12:52:51 -0600

    Ejercicio de web server con ESP32

--- arduino/ESPsyncWebServer/ESPsyncWebServer.ino
+++ arduino/ESPsyncWebServer/ESPsyncWebServer.ino
@@ -1,14 +1,22 @@
 
-#include<WiFi.h>
-#include<ESPAsynWebServer.h>
+#include <Arduino.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebSrv.h>
 
-const char* ssid="dr-rojano";
-const char* password="78928-tw";
+const char* ssid="Manu S21";
+const char* password="xapp6304";
 AsyncWebServer server(80);
 
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(2, OUTPUT);  
   Serial.begin(115200);
   conectarse();
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -17,13 +25,21 @@ void setup() {
     if(numParametros == 0){
       request -> send(200,"text/html","<h1>hola mundo</h1>");
     }else{
-            AsyncWebServer *p = request -> getParam(0);
+            AsyncWebParameter *p = request -> getParam(0);
             String html = "<h1>Hola " + p->value() + " desde ESP32</h1>";
             request -> send(200,"text/html", html);
     }
   });
-  server.on("/adios", HTTP_GET[](AsyncServerRequest *r){
+  server.on("/adios", HTTP_GET,[](AsyncWebServerRequest *r){
     r->send(200, "text/html", "<h1>adios</h1>");
+  });
+  server.on("/on", HTTP_GET,[](AsyncWebServerRequest *r){
+    r->send(200, "text/html", "<h1>prendido</h1>");
+    digitalWrite(2, HIGH);    
+  });  
+  server.on("/off", HTTP_GET,[](AsyncWebServerRequest *r){
+    r->send(200, "text/html", "<h1>apagado</h1>");
+    digitalWrite(2, LOW);
   });
   server.begin();
   
@@ -31,7 +47,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  void conectarse(){
+  
+}
+
+void conectarse(){
     Serial.print("Conectando");
     Serial.println(ssid);
     WiFi.begin(ssid,password);
@@ -41,4 +60,3 @@ void loop() {
     }
     Serial.print(WiFi.localIP());
   }
-}
